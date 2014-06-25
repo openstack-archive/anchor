3eefba3c9321ef7c3736c5ce1ea6dd877ef5a232
Stanislaw Pitucha <stanislaw.pitucha@hp.com>
2014-06-25 14:20:22 +0100

    Add logging for signing errors

--- ephemeral_ca/controllers/__init__.py
+++ ephemeral_ca/controllers/__init__.py
@@ -1,6 +1,9 @@
 from pecan import expose, request, response
 from .. import auth, certificate_ops, validators
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class RootController(object):
 
@@ -19,22 +22,26 @@ class RootController(object):
 
         auth_result = auth.validate(request.POST.get('user'), request.POST.get('secret'))
         if auth_result is auth.AUTH_FAILED:
+            logger.info("request failed authentication")
             response.status_int = 401
             return
 
         csr = certificate_ops.parse_csr(request.POST.get('csr'), request.POST.get('encoding'))
         if csr is None:
+            logger.info("csr in the request cannot be parsed")
             response.status_int = 400
             return 'CSR cannot be parsed\n'
 
         try:
             certificate_ops.validate_csr(auth_result, csr)
         except validators.ValidationError as e:
+            logger.exception("csr failed validation")
             response.status_int = 409
             return 'Validation failed: %s\n' % e
 
         cert = certificate_ops.sign(csr)
         if not cert:
+            logger.error("certificate signing error")
             response.status_int = 500
             return 'Signing Failure\n'
     
