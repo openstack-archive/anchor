cd009807f5f2feab59b3841b663c3fb896be0af8
Nathaniel Dillon <nathaniel.dillon@hp.com>
2015-02-17 16:17:23 -0800

    Adding check for domains not starting with a period
    Changes:
    * added check in app.py to error if cn doesn't start with a .
    * added unit test to check domain
    * removing unused imports, comments, and vars
    
    Change-Id: I5c5d3fad1204a0d7e3746ce4d6aa4d2c4775f10c
    Closes-Bug: #1422931

--- anchor/app.py
+++ anchor/app.py
@@ -54,6 +54,10 @@ def validate_config(conf):
                 raise ConfigValidationException("Validator set <%s> contains "
                                                 "an unknown validator <%s>",
                                                 name, step[0])
+    for domain in allowed_domains:
+        if not domain.startswith('.'):
+            raise ConfigValidationException("Domain that does not start with "
+                                            "a '.' <%s>", j)
 
 
 def setup_app(config):
