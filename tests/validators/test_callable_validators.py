64e5ef8cabbe51c739c4721936b115ff48f3bca8
Robert Clark <hyakuhei@gmail.com>
2015-03-11 18:48:27 +0000

    Added tests to bring coverage up to 100% of validators
    Change-Id: Ic0ab55f2f8c8c4a1be0dffcac47ecfd8d3108cf9

--- tests/validators/test_callable_validators.py
+++ tests/validators/test_callable_validators.py
@@ -119,6 +119,80 @@ class TestValidators(unittest.TestCase):
         DNS:server1.test.com, DNS:baddomain.com, IP Address:10.1.1.1
     """
 
+    csr_data_server_group_nv = textwrap.dedent("""
+        -----BEGIN CERTIFICATE REQUEST-----
+        MIIEuTCCAqECAQAwdDELMAkGA1UEBhMCR0IxDjAMBgNVBAgTBVdhbGVzMRQwEgYD
+        VQQHEwtBYmVyeXN0d3l0aDENMAsGA1UEChMET1NTRzENMAsGA1UECxMET1NTRzEh
+        MB8GA1UEAxMYbnYtMDAxLmNsb3VkLmV4YW1wbGUub3JnMIICIjANBgkqhkiG9w0B
+        AQEFAAOCAg8AMIICCgKCAgEA2ydAO8+Al22nwC6qxOzwfUUyDZzy4Wt5I2YXWniG
+        3qCid0nscLezFxqsdjVSh/Vg9n5vXVBcgzXccojjNgTRzb8lNrT17db7huc/fWkn
+        RBnweSb8qYUauJULV01T/xo4SLklLHQAvUqT+qEttRps6UnD0mQATfdmdPOnfRXM
+        NOH90Y7zsBrzjegU7h/fSPWsT2r2Mxii4MlW4KfW5YPW+kWrgPMhd3MTbZmL26Le
+        SnoOuji4PtQCaINkk90HTku8vy/rYI79umqReh7tuf5N6WoZcv6fAUINtDtLlVV0
+        tD7slPzVzv1JpACfVUO8NTIAthIxYmGBY5J5mu4Rd4chk6WY7TjMAO3I1CyHaQ2r
+        wPy5+u0o4DmIFDsc17iVL4Hn9lq70FaaRQ39+Xx6ldAmAsc2wqJhLNtRKrhBKaX4
+        Wst8hndz79DOTnRsK1jbbPyjIzAaeWquCby9nuCTDNr8HxYzKzmbEhdEjvukz26r
+        3vRIRmY2sF8n/GWy8AjYeSGUIn+a8Y4IpGyOBz1JrzCsgjtLDD+gZxvbv225KcLB
+        3G6gAS2c4UJPaHXpkrH8PgfKEW87iZdXTJa9Rm/yPKQSO22K5Y8AcIUv2jpZNi1G
+        e0koS5lg0ys7dD4zm3XqdTKTdAp5gxMZj8L+MwmBYasqThf95mdtMInWTNNqfscv
+        rY8CAwEAAaAAMA0GCSqGSIb3DQEBBQUAA4ICAQCCla5MeZqQAkKipVHB4LSDm46u
+        YEC6+JluS1S3SjyZAS3uaI7HOTy7lU9A3KtEKJLHHd+CBO0qNIHObx+ZDgA3oOtx
+        xLD+J626195U+TRKyB0q8YmmBOOaCjGPC0rBJEaLVUvt5zA9x21XCZHmrL8UsoHd
+        YEiGUCK+NxxXnQ5TxsPydmwSPcONZb6BWpJxwXn8+DFhSdSFH1uQCFMLyRSPQ39u
+        vWTlqz6hYQK72WgdUCq+72Gx44dBwW6L/eDZKX0i12lHWj0UoVsvmOWcfsYe+PTm
+        IDLbXtfz/ZzFUb05OeqLEaFn3ZsdN8IFw765H2VVCd1yl0LTEa/sMTFmF/resh2w
+        Qt+3aX0WgeADbT0fHxOS9K6chm4qEB+kKsX5xvUN5bPQvotahnmd+TsKStZKE/iF
+        X+NreOxH0DocfAKDHEXfKwzdx2+V5YBMo9Bimj9+1nlHivpf27v8hpw8C0u0xFfs
+        9uZX8A5Ys63yvuGTpUweB4l5EJxdLWgv2oqYo6SkeXEkjCbc3QoDeUj99m3vmYrm
+        U5EtGzSWrhAHFE3I4IzQT0lz+5+pMUI4K/baRHQdEseWMe41E4sibku+6CkGnFVH
+        1YKiEDMfGlChrL4tJ9yEr+UZJnA5PlwUDgsq0H0U555t7x7IQuFrlXq6KwzesMD1
+        eMhhd5I4DZwYTks4wg==
+        -----END CERTIFICATE REQUEST-----""")
+    """
+    Subject: C=GB, ST=Wales, CN=nv-001.cloud.example.org
+    """
+
+    csr_data_key_usage = textwrap.dedent("""
+        -----BEGIN CERTIFICATE REQUEST-----
+        MIIE+DCCAuACAQAwXTELMAkGA1UEBhMCR0IxEzARBgNVBAgMClNvbWUtU3RhdGUx
+        DTALBgNVBAoMBE9TU0cxDTALBgNVBAsMBE9TU0cxGzAZBgNVBAMMEnNlcnZlci5l
+        eGFtcGxlLm9yZzCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAJWMqj5k
+        B+wNzBMNYJ9tHIYDgxb8c/IqKEcDf35VzuYr3O2/gYalFlmQnz0LRQRaFVOjBKGh
+        hVAxSe4fS9tlcchiImG/2iFsFUbDufI4oq7bYuMEWuW5McJBTi+2sxqxmdwikgqr
+        LlEstDjHOuGL1YYHhlBfEvW2v6rIaCRPL9MGVO/KjAkXqmD3LAX2vCMjDHjI0XZR
+        KkatQcIAy7SNY42O5uWJm+ow/Zn/tVqbRQnICTcKiS0o5oIKVyqUvhJuiFzHSi0y
+        A3wAb7c70wKD7/f98Y09AoZS9eV1UGD04uihZpB4i/cfsQpcyvm7c2DGycHJPSm9
+        q7wXYUj7F0nvFT+eb+kigTSKGClDcrbxB9XTssN+KCdg1mOiFXqPDV68XEb3YZOe
+        +tpAtqJLmTPXvqLdVZC9EmnybXOONgWITzKQL2HwI4pV9riMxJV1ZzRDIlkKq43p
+        jmcKN9DFBxzpQ4X8xd+UNyV5lAp5iXtIaJMaanMs682eEPtJ7xLz6gwVbZsiUoLo
+        RYcw+tFuE4QtwNGN3FrFIVJ8VgV2QahWIcVcEDGQZXdgz4eWGFB9XemxG/UP8T3c
+        APGRGQBNXmR4Bp0hBkE1G9aTEF6ICvdU3+KaQjPz7RtafCdxMEpwyebQCOzTHsom
+        Nh/9kl+DYQ3OfYq9G83OB67/nEEii9daOD5lAgMBAAGgVjBUBgkqhkiG9w0BCQ4x
+        RzBFMAkGA1UdEwQCMAAwCwYDVR0PBAQDAgXgMCsGA1UdEQQkMCKCD3d3dy5leGFt
+        cGxlLm5ldIIPd3d3LmV4YW1wbGUub3JnMA0GCSqGSIb3DQEBBQUAA4ICAQB/vy68
+        MNc9avJSQPN0masOYp4uqJ9Q+iRkM/fYQxHQXdJMWbBfCVPiIOXEy4foPAxLG0GI
+        YTBpS9hpXj7yM95O2Pd52DHOe8CJ2LT3L/qbVnximVryRFHjN/7EDaoI9JwUHDLJ
+        yb934yogIgYCRwNHYgjeHw2XjdmZj11ZS9JXuIdJavGs47DveoHp5igOUJ2iXhVz
+        09+ocEVm1d6d7bddOLjWuNzmtATUmiI0kjjkxaqet0uKSVmfR+pZDoDmSxpsuMqG
+        dxrWkWFVTzC+P+zGTAQPThV9rE/McZ4Bg5J1fkV8VKi2JIycMlNqo/5AS0jrDQXl
+        iBxEnRWZ1gh5p0dNhPm+hU84xsU3V9TbR6KCm4NEqc+mrMX1WFHIlLLN3eE8RiKh
+        iKpHtPZJfiYgnDXzdvH+JW8gS0s7u8jwi87UhOp/bJ0T7eFkVYtL3wg7osme9G81
+        A5mnMPiDzIrHBf7tnWCGhebqfcYgGFO1P+xU7Ak0dyL2n+I+8jYMkBzm3TibRNOt
+        pNB/psKMsZ3le3V8v7BjYrQ00uGE4g0uIZrVoIdXGvmAllw8kyvicC/q2qwlJbAM
+        rxQFbcyfpMtv1t+3PODaOdnxayLBHp6AtQVv/4pa5DS6Lhr+Ggfn5h6WVs5VIojj
+        0SZgjmhfdiQJlxSOYFO14GlPocD7xZ69dypV/w==
+        -----END CERTIFICATE REQUEST-----""")
+    """
+    Subject: U=OSSG, CN=server.example.org
+    Requested Extensions:
+        X509v3 Basic Constraints:
+            CA:FALSE
+        X509v3 Key Usage:
+            Digital Signature, Non Repudiation, Key Encipherment
+        X509v3 Subject Alternative Name:
+            DNS:www.example.net, DNS:www.example.org
+    """
+
     def setUp(self):
         super(TestValidators, self).setUp()
 
@@ -133,6 +207,14 @@ class TestValidators(unittest.TestCase):
         self.csr_good_sub_alt_names_IP = signing_request.X509Csr()
         self.csr_good_sub_alt_names_IP.from_buffer(
             TestValidators.csr_data_good_subjectAltNames_IP)
+
+        self.csr_server_group_nv = signing_request.X509Csr()
+        self.csr_server_group_nv.from_buffer(
+            TestValidators.csr_data_server_group_nv)
+
+        self.csr_key_usage = signing_request.X509Csr()
+        self.csr_key_usage.from_buffer(
+            TestValidators.csr_data_key_usage)
 
     def tearDown(self):
         super(TestValidators, self).tearDown()
@@ -232,6 +314,19 @@ class TestValidators(unittest.TestCase):
             )
         )
 
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    @mock.patch('anchor.X509.certificate.X509Extension.get_name')
+    def test_alternative_names_ext(self, get_name, get_value):
+        get_name.return_value = "subjectAltName"
+        get_value.return_value = "BAD,10.1.1.1"
+
+        self.assertRaises(
+            validators.ValidationError,
+            validators.alternative_names,
+            self.csr_good_sub_alt_names,
+            allowed_domains=['.test.com']
+        )
+
     @mock.patch('socket.gethostbyname_ex')
     def test_alternative_names_bad_domains(self, gethostbyname_ex):
         gethostbyname_ex.return_value = ('master.test.com', [], ['10.0.0.1'])
@@ -265,6 +360,19 @@ class TestValidators(unittest.TestCase):
             allowed_networks=['99/8']
         )
 
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    @mock.patch('anchor.X509.certificate.X509Extension.get_name')
+    def test_alternative_names_IP_ext(self, get_name, get_value):
+        get_name.return_value = "subjectAltName"
+        get_value.return_value = "BAD,10.1.1.1"
+
+        self.assertRaises(
+            validators.ValidationError,
+            validators.alternative_names_ip,
+            self.csr_good_sub_alt_names,
+            allowed_domains=['.test.com']
+        )
+
     @mock.patch('anchor.X509.name.X509Name.Entry.get_name')
     def test_alternative_names_ip_bad_ext(self, get_name):
         get_name.return_value = ('BAD:VALUE')
@@ -274,4 +382,173 @@ class TestValidators(unittest.TestCase):
             self.csr_good_sub_alt_names_IP,
             allowed_domains=['mock'],
             allowed_networks=['99/8']
+        )
+
+    def test_server_group_admin(self):
+        # Pass in a CSR without a - prefix
+        self.assertEqual(
+            None,
+            validators.server_group(
+                auth_result=None,
+                csr=self.csr_good_sub_alt_names,
+                group_prefixes={}
+            )
+        )
+
+        self.assertEqual(
+            None,
+            validators.server_group(
+                auth_result=None,
+                csr=self.csr_server_group_nv,
+                group_prefixes={}
+            )
+        )
+
+        # 'nv' in prefix means only Nova members should be able to issue
+        auth_result = mock.Mock()
+        auth_result.groups = ['nova']
+        self.assertEqual(
+            None,
+            validators.server_group(
+                auth_result=auth_result,
+                csr=self.csr_server_group_nv,
+                group_prefixes={'nv': 'nova', 'sw': 'swift'}
+            )
+        )
+
+    def test_server_group_bad(self):
+        auth_result = mock.Mock()
+        auth_result.groups = ['glance']
+        self.assertRaises(
+            validators.ValidationError,
+            validators.server_group,
+            auth_result=auth_result,
+            csr=self.csr_server_group_nv,
+            group_prefixes={'nv': 'nova', 'sw': 'swift'}
+        )
+
+    @mock.patch('anchor.X509.certificate.X509Extension.get_name')
+    def test_extensions(self, get_name):
+        self.assertEqual(
+            None,
+            validators.extensions(csr=self.csr_server_group_nv)
+        )
+
+        get_name.return_value = 'BAD'
+        self.assertRaises(
+            validators.ValidationError,
+            validators.extensions,
+            csr=self.csr_good_sub_alt_names,
+            allowed_extensions=['GOOD-1', 'GOOD-2']
+        )
+
+    def test_key_usage(self):
+        # Digital Signature, Non Repudiation, Key Encipherment
+        allowed_usage = ['Digital Signature',
+                         'Non Repudiation',
+                         'Key Encipherment']
+
+        self.assertEqual(
+            None,
+            validators.key_usage(
+                csr=self.csr_key_usage,
+                allowed_usage=allowed_usage
+            )
+        )
+
+        self.assertRaises(
+            validators.ValidationError,
+            validators.key_usage,
+            csr=self.csr_key_usage,
+            allowed_usage=['Not This']
+        )
+
+    def test_ca_status(self):
+        self.assertEqual(
+            None,
+            validators.ca_status(
+                csr=self.csr_key_usage,
+                ca_requested=False
+            )
+        )
+
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    def test_ca_status_bad_format(self, get_value):
+        get_value.return_value = "BAD:STRING:FORMAT"
+        self.assertRaises(
+            validators.ValidationError,
+            validators.ca_status,
+            csr=self.csr_key_usage,
+            ca_requested=False
+        )
+
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    def test_ca_status_ca_false(self, get_value):
+        get_value.return_value = "CA:TRUE"
+        self.assertRaises(
+            validators.ValidationError,
+            validators.ca_status,
+            csr=self.csr_key_usage,
+            ca_requested=False
+        )
+
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    def test_ca_status_pathlen(self, get_value):
+        get_value.return_value = "pathlen:something"
+        self.assertEqual(
+            None,
+            validators.ca_status(
+                csr=self.csr_key_usage,
+                ca_requested=False
+            )
+        )
+
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    def test_ca_status_bad_value(self, get_value):
+        get_value.return_value = 'bad:value'
+        self.assertRaises(
+            validators.ValidationError,
+            validators.ca_status,
+            csr=self.csr_key_usage,
+            ca_requested=False
+        )
+
+    @mock.patch('anchor.X509.certificate.X509Extension.get_name')
+    @mock.patch('anchor.X509.certificate.X509Extension.get_value')
+    def test_ca_status_key_usage(self, get_value, get_name):
+        get_name.return_value = 'keyUsage'
+        get_value.return_value = 'Certificate Sign'
+        self.assertRaises(
+            validators.ValidationError,
+            validators.ca_status,
+            csr=self.csr_key_usage,
+            ca_requested=False
+        )
+
+    def test_source_cidrs_good(self):
+        request = mock.Mock(client_addr='127.0.0.1')
+        self.assertEqual(
+            None,
+            validators.source_cidrs(
+                request=request,
+                cidrs=['127/8', '10/8']
+            )
+        )
+
+    def test_source_cidrs_out_of_range(self):
+        request = mock.Mock(client_addr='99.0.0.1')
+        self.assertRaises(
+            validators.ValidationError,
+            validators.source_cidrs,
+            request=request,
+            cidrs=['127/8', '10/8']
+        )
+
+    def test_source_cidrs_bad_cidr(self):
+        request = mock.Mock(client_addr='127.0.0.1')
+        self.assertRaises(
+            validators.ValidationError,
+            validators.source_cidrs,
+            request=request,
+            cidrs=['bad']
         )
