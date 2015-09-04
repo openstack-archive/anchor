e6044914ee2b2fde8ad0e4a7cf23c44c722599d5
Stanisław Pitucha <stanislaw.pitucha@hp.com>
2015-09-04 17:08:56 +1000

    Add rfc based validators
    Add a validator which collects various standard format/behaviour tests.
    These are not user-configurable and any valid request failing them is a
    bug in Anchor.
    
    All checks reference the document where they're defined.
    
    Closes-bug: 1476877
    Partial-bug: 1476875
    Change-Id: I208685d8d7cde40ed5294e7235d64ca17617c094

---
+++ anchor/validators_standards.py
@@ -0,0 +1,99 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Standards based validator.
+
+This module provides validators which should be included in all deployments and
+which are based directly on the standards documents. All exceptions must have a
+comment referencing the document / section they're based on.
+
+All the rules are pulled into a single validator: ``standards_compliance``.
+"""
+
+from __future__ import absolute_import
+
+import re
+
+from anchor import validators
+from anchor.X509 import extension
+
+
+def standards_compliance(csr=None, **kwargs):
+    """Collection of separate cases of standards validation."""
+    _no_extension_duplicates(csr)
+    _critical_flags(csr)
+    _valid_domains(csr)
+    # TODO(stan): validate srv/uri, distinct DNs, email format, identity keys
+
+
+def _no_extension_duplicates(csr):
+    """Only one extension with a given oid is allowed.
+
+    See RFC5280 section 4.2
+    """
+    seen_oids = set()
+    for ext in csr.get_extensions():
+        oid = ext.get_oid()
+        if oid in seen_oids:
+            raise validators.ValidationError(
+                "Duplicate extension with oid %s" % oid)
+        seen_oids.add(oid)
+
+
+def _critical_flags(csr):
+    """Various rules define whether critical flag is required."""
+    for ext in csr.get_extensions():
+        if isinstance(ext, extension.X509ExtensionSubjectAltName):
+            # RFC5280 section 4.1.2.6
+            if len(csr.get_subject()) == 0 and not ext.get_critical():
+                raise validators.ValidationError(
+                    "SAN must be critical if subject is empty")
+        if isinstance(ext, extension.X509ExtensionBasicConstraints):
+            # RFC5280 section 4.1.2.9
+            if not ext.get_critical():
+                raise validators.ValidationError(
+                    "Basic constraints has to be marked critical")
+
+
+# RFC1034 allows a simple " " too, but it's not allowed in certificates, so it
+# will not match
+RE_DOMAIN_LABEL = re.compile("^[a-z](?:[-a-z0-9]*[a-z0-9])?$", re.IGNORECASE)
+
+
+def _valid_domains(csr):
+    """Format of the domin names
+
+    See RFC5280 section 4.2.1.6
+    """
+    def check_domains(hostname):
+        # See RFC1034 section 3.5
+        labels = hostname.split('.')
+        if labels[-1] == "":
+            # single trailing . is ok, ignore
+            labels.pop(-1)
+        for label in labels:
+            if len(label) > 63:
+                return False
+            if RE_DOMAIN_LABEL.match(label) is None:
+                return False
+        return True
+
+    sans = csr.get_extensions(extension.X509ExtensionSubjectAltName)
+    if not sans:
+        return
+    ext = sans[0]
+    for domain in ext.get_dns_ids():
+        if not check_domains(domain):
+            raise validators.ValidationError(
+                "SAN entry <%s> is not a valid domain" % (domain,))
---
+++ tests/validators/test_standards_validator.py
@@ -0,0 +1,170 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import textwrap
+import unittest
+
+from pyasn1.codec.der import encoder
+from pyasn1_modules import rfc2459
+
+from anchor import validators
+from anchor import validators_standards
+from anchor.X509 import extension
+from anchor.X509 import name
+from anchor.X509 import signing_request
+
+
+class TestStandardsValidator(unittest.TestCase):
+    csr_data = textwrap.dedent(u"""
+        -----BEGIN CERTIFICATE REQUEST-----
+        MIIB1TCCAT4CAQAwgZQxCzAJBgNVBAYTAlVLMQ8wDQYDVQQIDAZOYXJuaWExEjAQ
+        BgNVBAcMCUZ1bmt5dG93bjEXMBUGA1UECgwOQW5jaG9yIFRlc3RpbmcxEDAOBgNV
+        BAsMB3Rlc3RpbmcxFDASBgNVBAMMC2FuY2hvci50ZXN0MR8wHQYJKoZIhvcNAQkB
+        FhB0ZXN0QGFuY2hvci50ZXN0MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCe
+        eqg1Qeccv8hqj1BP9KEJX5QsFCxR62M8plPb5t4sLo8UYfZd6kFLcOP8xzwwvx/e
+        FY6Sux52enQ197o8aMwyP77hMhZqtd8NCgLJMVlUbRhwLti0SkHFPic0wAg+esfX
+        a6yhd5TxC+bti7MgV/ljA80XQxHH8xOjdOoGN0DHfQIDAQABoAAwDQYJKoZIhvcN
+        AQELBQADgYEAI4eMihRKSeNLt1DLg6l+WYU4ssRTEHpxwBRo0lh5IGEBjtL+NrPY
+        /A9AKfbkyW7BnKd9IT5wvenZajl5UzCveTCkqVDbSEOwLpUY3GeHf0jujml8gKFb
+        AFrlaOkOuDai+an0EdbeLef1kYh8CWd573MPvKTwOsiaGP/EACrlIEM=
+        -----END CERTIFICATE REQUEST-----""")
+
+    def test_passing(self):
+        csr = signing_request.X509Csr.from_buffer(self.csr_data)
+        validators_standards.standards_compliance(csr=csr)
+
+
+class TestExtensionDuplicates(unittest.TestCase):
+    def test_no_extensions(self):
+        csr = signing_request.X509Csr()
+        validators_standards._no_extension_duplicates(csr)
+
+    def test_no_duplicates(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        csr.add_extension(ext)
+        validators_standards._no_extension_duplicates(csr)
+
+    def test_with_duplicates(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id('example.com')
+        exts = rfc2459.Extensions()
+        exts[0] = ext._ext
+        exts[1] = ext._ext
+        # Anchor doesn't allow this normally, so tests need to cheat
+        attrs = csr.get_attributes()
+        attrs[0] = None
+        attrs[0]['type'] = signing_request.OID_extensionRequest
+        attrs[0]['vals'] = None
+        attrs[0]['vals'][0] = encoder.encode(exts)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._no_extension_duplicates(csr)
+
+
+class TestExtensionCriticalFlags(unittest.TestCase):
+    def test_no_subject_san_not_critical(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.set_critical(False)
+        ext.add_dns_id('example.com')
+        csr.add_extension(ext)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._critical_flags(csr)
+
+    def test_no_subject_san_critical(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.set_critical(True)
+        ext.add_dns_id('example.com')
+        csr.add_extension(ext)
+        validators_standards._critical_flags(csr)
+
+    def test_with_subject_san_not_critical(self):
+        csr = signing_request.X509Csr()
+        subject = name.X509Name()
+        subject.add_name_entry(name.OID_commonName, "example.com")
+        csr.set_subject(subject)
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.set_critical(False)
+        ext.add_dns_id('example.com')
+        csr.add_extension(ext)
+        validators_standards._critical_flags(csr)
+
+    def test_basic_constraints_not_critical(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionBasicConstraints()
+        ext.set_critical(False)
+        csr.add_extension(ext)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._critical_flags(csr)
+
+    def test_basic_constraints_critical(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionBasicConstraints()
+        ext.set_critical(True)
+        csr.add_extension(ext)
+        validators_standards._critical_flags(csr)
+
+
+class TestValidDomains(unittest.TestCase):
+    def test_all_valid(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id('a-123.example.com')
+        csr.add_extension(ext)
+        validators_standards._valid_domains(csr)
+
+    def test_all_valid_trailing_dot(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id('a-123.example.com.')
+        csr.add_extension(ext)
+        validators_standards._valid_domains(csr)
+
+    def test_too_long(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id('very-long-label-over-63-characters-'
+                       'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.example.com')
+        csr.add_extension(ext)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._valid_domains(csr)
+
+    def test_beginning_hyphen(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id('-label.example.com')
+        csr.add_extension(ext)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._valid_domains(csr)
+
+    def test_trailing_hyphen(self):
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id('label-.example.com')
+        csr.add_extension(ext)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._valid_domains(csr)
+
+    def test_san_space(self):
+        # valid domain, but not in CSRs
+        csr = signing_request.X509Csr()
+        ext = extension.X509ExtensionSubjectAltName()
+        ext.add_dns_id(' ')
+        csr.add_extension(ext)
+        with self.assertRaises(validators.ValidationError):
+            validators_standards._valid_domains(csr)
