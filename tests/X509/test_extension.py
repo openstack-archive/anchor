8b9d55f2b2497585e09cd7c2d618a9860312f0fc
Stanisław Pitucha <stanislaw.pitucha@hp.com>
2015-10-15 11:27:00 +1100

    Add mising extensions tests
    Fixes broken nameconstraints methods found in the process.
    
    Change-Id: Iacc43b0a42416b0b9b5dac3c0c738c0a817261b1

--- tests/X509/test_extension.py
+++ tests/X509/test_extension.py
@@ -17,6 +17,7 @@
 import unittest
 
 import netaddr
+from pyasn1.codec.der import encoder
 from pyasn1_modules import rfc2459  # X509v3
 
 from anchor.X509 import errors
@@ -61,6 +62,14 @@ class TestExtensionBase(unittest.TestCase):
         self.assertFalse(ext.get_critical())
         ext.set_critical(True)
         self.assertTrue(ext.get_critical())
+
+    def test_serialise(self):
+        asn1 = rfc2459.Extension()
+        asn1['extnID'] = rfc2459.univ.ObjectIdentifier('1.2.3.4')
+        asn1['critical'] = False
+        asn1['extnValue'] = "foobar"
+        ext = extension.construct_extension(asn1)
+        self.assertEqual(ext.as_der(), encoder.encode(asn1))
 
 
 class TestBasicConstraints(unittest.TestCase):
@@ -169,6 +178,18 @@ class TestNameConstraints(unittest.TestCase):
         self.assertEqual(1, self.ext.get_permitted_length())
         self.assertEqual(1, self.ext.get_excluded_length())
 
+    def test_excluded(self):
+        self.ext.add_excluded('dNSName', 'example.com')
+        self.assertEqual(self.ext.get_excluded_range(0), (0, None))
+        self.assertEqual(self.ext.get_excluded_name(0),
+                         ('dNSName', b'example.com'))
+
+    def test_permitted(self):
+        self.ext.add_permitted('dNSName', 'example.com')
+        self.assertEqual(self.ext.get_permitted_range(0), (0, None))
+        self.assertEqual(self.ext.get_permitted_name(0),
+                         ('dNSName', b'example.com'))
+
 
 class TestExtendedKeyUsage(unittest.TestCase):
     def setUp(self):
@@ -192,6 +213,8 @@ class TestExtendedKeyUsage(unittest.TestCase):
         self.assertEqual(1, len(self.ext.get_all_usages()))
         self.ext.set_usage(rfc2459.id_kp_clientAuth, True)
         self.assertEqual(1, len(self.ext.get_all_usages()))
+        self.ext.set_usage(rfc2459.id_kp_codeSigning, True)
+        self.assertEqual(2, len(self.ext.get_all_usages()))
 
     def test_unset(self):
         self.ext.set_usage(rfc2459.id_kp_clientAuth, True)
@@ -206,3 +229,9 @@ class TestExtendedKeyUsage(unittest.TestCase):
         self.assertEqual(
             "extKeyUsage: TLS Web Client Authentication, Code Signing",
             str(self.ext))
+
+    def test_invalid_usage(self):
+        self.assertRaises(ValueError, self.ext.get_usage,
+                          rfc2459.univ.ObjectIdentifier('1.2.3.4'))
+        self.assertRaises(ValueError, self.ext.set_usage, True,
+                          rfc2459.univ.ObjectIdentifier('1.2.3.4'))
